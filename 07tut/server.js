c065e926bb2f60775b40ab0e4c8893f2ec7d905e
YamiKapil <yamikapil@gmail.com>
2023-08-15 22:50:23 +0545

    started express middleware

--- 07tut/server.js
+++ 07tut/server.js
@@ -1,7 +1,48 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
 const PORT = process.env.PORT || 3500;
+
+// custom logger middleware...
+// app.use((req, res, next) => {
+//   logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, "reqLog.txt");
+//   console.log(`${req.method} ${req.path}`);
+//   next();
+// });
+/// now we can just use
+app.use(logger);
+
+/// cors stands for Cross Origin Resource Sharing..
+const whitelist = [
+  "https://www.mysite.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+];
+const corsOptions = {
+  orgin: (origin, callback) => {
+    if (whitelist.indexOf(origin) != -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by Cors"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// build in middleware to handle urlendoed data
+// inother words, form data:
+// 'content-type: application/x-www-form-urlencoded'
+// used for the formData..
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("^/$|/index(.html)?", (req, res) => {
   //   res.send("Hello World!");
